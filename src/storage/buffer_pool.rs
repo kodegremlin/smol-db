82a29a72a33e8dd43f1f001299821107f236c1f1
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-07-19 12:43:03 +0530

    refactor: some canonical name changes to better reflect what they are referred to in database theory

--- src/storage/buffer_pool.rs
+++ src/storage/buffer_pool.rs
@@ -11,35 +11,49 @@ use crate::{
     },
 };
 
+/// Decouples the memory manager from the physical Wal implementation.
+pub trait WalFlusher: Send + Sync {
+    /// Forces the Wal manager to synchronously write and fsync all
+    /// log records up-to and including the specified lsn, out to
+    /// the non-volatile disk.
+    fn flush_upto(&self, lsn: u64) -> Result<(), DbError>;
+}
+
 /// A thread-safe referance to a cached page. The `Arc` provides lifecycle
 /// tracking (pinning), and the `RwLock` provides per page "latching".
-pub type PageRef = Arc<RwLock<BTreeNode>>;
+pub type Frame = Arc<RwLock<BTreeNode>>;
 
 /// Handles memory caching, page fetching, and eviction.
 pub struct BufferPool {
     disk_manager: DiskManager,
     replacer: LruReplacer,
-    page_table: HashMap<PageId, PageRef>,
+    page_table: HashMap<PageId, Frame>,
     capacity: usize,
+    wal_flusher: Option<Arc<dyn WalFlusher>>,
 }
 
 impl BufferPool {
     /// Creates a new BufferPool with a specified capacity.
-    pub fn new(disk_manager: DiskManager, capacity: usize) -> Self {
+    pub fn new(
+        disk_manager: DiskManager,
+        capacity: usize,
+        wal_flusher: Option<Arc<dyn WalFlusher>>,
+    ) -> Self {
         Self {
             disk_manager,
             replacer: LruReplacer::new(capacity),
             page_table: HashMap::with_capacity(capacity),
             capacity,
+            wal_flusher,
         }
     }
 
     /// Fetches a page from the buffer pool. If it's a cache miss, it reads
     /// from disk, potentially evicting an old page.
-    pub fn fetch_page(&mut self, page_id: PageId) -> Result<PageRef, DbError> {
-        if let Some(page_ref) = self.page_table.get(&page_id) {
+    pub fn fetch_page(&mut self, page_id: PageId) -> Result<Frame, DbError> {
+        if let Some(frame) = self.page_table.get(&page_id) {
             self.replacer.record_access(page_id);
-            return Ok(page_ref.clone());
+            return Ok(frame.clone());
         }
 
         // cache miss: we'll have to load the page from disk.
@@ -51,34 +65,34 @@ impl BufferPool {
         let raw_page = self.disk_manager.read_page(&page_id)?;
         let node = BTreeNode::decode(&raw_page)?;
 
-        let page_ref = Arc::new(RwLock::new(node));
+        let frame = Arc::new(RwLock::new(node));
 
-        self.page_table.insert(page_id, page_ref.clone());
+        self.page_table.insert(page_id, frame.clone());
         self.replacer.record_access(page_id);
 
-        Ok(page_ref)
+        Ok(frame)
     }
 
     /// Allocates a completely new page via the `DiskManager` and adds it to the pool.
-    pub fn new_page(&mut self, is_leaf: bool) -> Result<(PageId, PageRef), DbError> {
+    pub fn new_page(&mut self, is_leaf: bool) -> Result<(PageId, Frame), DbError> {
         if self.page_table.len() >= self.capacity {
             self.evict_page()?;
         }
         let page_id = self.disk_manager.allocate_page();
 
         let node = BTreeNode::new_empty(page_id, is_leaf);
-        let page_ref = Arc::new(RwLock::new(node));
+        let frame = Arc::new(RwLock::new(node));
 
-        self.page_table.insert(page_id, page_ref.clone());
+        self.page_table.insert(page_id, frame.clone());
         self.replacer.record_access(page_id);
 
-        Ok((page_id, page_ref))
+        Ok((page_id, frame))
     }
 
     /// Flushes a specific page to disk if it is dirty.
     pub fn flush_page(&mut self, page_id: PageId) -> Result<(), DbError> {
-        if let Some(page_ref) = self.page_table.get(&page_id) {
-            let mut node_guard = page_ref
+        if let Some(frame) = self.page_table.get(&page_id) {
+            let mut node_guard = frame
                 .write()
                 .map_err(|_| DbError::CorruptPage("Poisoned lock".into()))?;
 
@@ -108,7 +122,7 @@ impl BufferPool {
         let evict_id = self
             .replacer
             .evict_if(|page_id| match self.page_table.get(page_id) {
-                Some(page_ref) => Arc::strong_count(page_ref) == 1,
+                Some(frame) => Arc::strong_count(frame) == 1,
                 None => {
                     panic!(
                         "LruReplacer contains PageId({:?}); should also be present in page_table",
